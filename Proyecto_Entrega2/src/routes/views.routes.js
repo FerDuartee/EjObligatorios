1833fca7fbb599defefcd2aecc0b15fe783eef7e
Fernando Duarte <progferduarte@gmail.com>
2024-02-25 13:40:46 -0300

    Preentregga - Version 4

--- Proyecto_Entrega2/src/routes/views.routes.js
+++ Proyecto_Entrega2/src/routes/views.routes.js
@@ -1,11 +1,42 @@
 const { Router } = require("express");
 const path = require('path');
 const ProductManager = require('../dao/fileSystem/ProductManager');
+const productsModel = require('../dao/mongoDb/models/products.model');
+const cartsModel = require('../dao/mongoDb/models/carts.models');
 
 const pathBase = path.join(__dirname, '../dao/fileSystem/productos.json');
 const productManager = new ProductManager(pathBase);
 
 const router = Router();
+
+// Ruta para mostrar todos los productos con paginación
+router.get('/products', async (req, res) => {
+    try {
+        const options = {
+            page: req.query.page || 1,
+            limit: req.query.limit || 10,
+            lean: true 
+        };
+
+        const products = await productsModel.paginate({}, options);
+        res.render('products', { products: products });
+    } catch (error) {
+        console.error('Error al obtener productos:', error);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    }
+});
+
+// Ruta para mostrar un carrito específico
+router.get('/carts/:cid', async (req, res) => {
+    const cartId = req.params.cid;
+    try {
+        const cart = await cartsModel.findById(cartId).populate('products.product').lean();
+        res.render('cart', { cart: cart });
+    } catch (error) {
+        console.error('Error al obtener productos del carrito:', error);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    }
+});
 
 router.get('/', async (req, res) => {
     try {
